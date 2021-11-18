c8b8f999a8f1665cba22d727d0101c6f18bfd282
kimbeom-u <gld55602@gmail.com>
2021-11-18 23:25:53 +0900

    PostsApiController

---
+++ src/main/java/com/spring/project/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.spring.project.service.posts;
+
+import com.spring.project.domain.posts.PostsRepository;
+import com.spring.project.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
