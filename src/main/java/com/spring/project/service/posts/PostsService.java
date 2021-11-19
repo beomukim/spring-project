0727a5b66a18467c196d184f206cb258d56e2caa
kimbeom-u <gld55602@gmail.com>
2021-11-19 15:37:14 +0900

    PostsApiControllerTest

--- src/main/java/com/spring/project/service/posts/PostsService.java
+++ src/main/java/com/spring/project/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.spring.project.service.posts;
 
+import com.spring.project.domain.posts.Posts;
 import com.spring.project.domain.posts.PostsRepository;
+import com.spring.project.web.dto.PostsResponseDto;
 import com.spring.project.web.dto.PostsSaveRequestDto;
+import com.spring.project.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +18,23 @@ public class PostsService {
     @Transactional
     public long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 
 }
