fe0781b05757a3c1a43121e97f4342c67f24ab96
kimbeom-u <gld55602@gmail.com>
2021-11-23 15:19:14 +0900

    Google oauth

--- src/main/java/com/spring/project/service/posts/PostsService.java
+++ src/main/java/com/spring/project/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.spring.project.service.posts;
 
 import com.spring.project.domain.posts.Posts;
 import com.spring.project.domain.posts.PostsRepository;
+import com.spring.project.web.dto.PostsListResponseDto;
 import com.spring.project.web.dto.PostsResponseDto;
 import com.spring.project.web.dto.PostsSaveRequestDto;
 import com.spring.project.web.dto.PostsUpdateRequestDto;
@@ -9,11 +10,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.lang.management.LockInfo;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class PostsService {
 
     private final PostsRepository postsRepository;
+
 
     @Transactional
     public long save(PostsSaveRequestDto requestDto) {
@@ -37,4 +43,18 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
 }
